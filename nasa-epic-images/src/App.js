dd7deed3a9fbea762c272589f1e5112e2f2a5a26
Marlon Rodriguez <mrbox30@gmail.com>
2020-11-24 00:59:46 -0700

    Add makeImageUrl method

--- nasa-epic-images/src/App.js
+++ nasa-epic-images/src/App.js
@@ -1,10 +1,19 @@
 import ImageContainer from "./ImageContainer";
 import Navigation from "./Navigation";
 
+const partialImageUrl = "https://epic.gsfc.nasa.gov/archive/natural/";
+
 function App() {
   const imageUrl = [
     "https://epic.gsfc.nasa.gov/archive/natural/2020/11/06/png/epic_1b_20201106002712.png",
   ];
+
+  // Make image url
+  const makeImageUrl = (year, month, day, imageId) => {
+    const url = partialImageUrl + `${year}/${month}/${day}/png/${imageId}.png`;
+    return url;
+  };
+
   return (
     <div>
       <Navigation />
