d67bcc6b3cb9b135096e8194064d518292341462
Marlon Rodriguez <mrbox30@gmail.com>
2020-11-24 17:24:21 -0700

    Add useEffect for dateData change and modify makeImageUrl method

--- nasa-epic-images/src/App.js
+++ nasa-epic-images/src/App.js
@@ -1,5 +1,6 @@
 import ImageContainer from "./ImageContainer";
 import Navigation from "./Navigation";
+import { useState, useEffect } from "react";
 
 const partialImageUrl = "https://epic.gsfc.nasa.gov/archive/natural/";
 const partialDateDataUlr = "https://epic.gsfc.nasa.gov/api/natural/date/";
@@ -7,12 +8,29 @@ const partialDateDataUlr = "https://epic.gsfc.nasa.gov/api/natural/date/";
 // oct 10:
 // epic_1b_20201106002712;
 function App() {
-  const imageUrl = [
-    "https://epic.gsfc.nasa.gov/archive/natural/2020/11/06/png/epic_1b_20201106002712.png",
-  ];
+  // const imageUrl = [
+  //   "https://epic.gsfc.nasa.gov/archive/natural/2020/11/06/png/epic_1b_20201106002712.png",
+  // ];
+  const [dateData, setDateData] = useState([]);
+  const [imageUrl, setImageUrl] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  // Hooks
+
+  // When dateData changes, make image url and set image url
+  useEffect(() => {
+    if (dateData.length) {
+      const url = makeImageUrl(dateData);
+      setImageUrl(url);
+    }
+  }, [dateData]);
 
   // Make image url
-  const makeImageUrl = (year, month, day, imageId) => {
+  const makeImageUrl = (dateData) => {
+    const year = dateData[0]["indentifier"].substring(0, 4);
+    const month = dateData[0]["indentifier"].substring(4, 6);
+    const day = dateData[0]["indentifier"].substring(6, 8);
+    const imageId = dateData[0]["image"];
     const url = partialImageUrl + `${year}/${month}/${day}/png/${imageId}.png`;
     return url;
   };
@@ -41,11 +59,17 @@ function App() {
 
   const handleDateChange = (date) => {
     // Fetch date data
+    const url = makeDateDataUrl(date);
+    const dateData = fetchDateData(url);
     // Set date data
+
     // Using date data, make image url
     // Set image url
   };
 
+  if (loading) {
+    return <h1>Loading</h1>;
+  }
   return (
     <div>
       <Navigation handleDateChange={handleDateChange} />
