6e194ee9b5867a56fd01120f5eba02cce465b5cb
Marlon Rodriguez <mrbox30@gmail.com>
2020-11-25 01:33:12 -0700

    Add Material-UI styling

--- nasa-epic-images/src/DateSelector.js
+++ nasa-epic-images/src/DateSelector.js
@@ -1,4 +1,11 @@
 import React from "react";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import { NativeSelect } from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
 
 function DateSelector({ availableDatesArray, handleDateChange }) {
   const handleOnchange = (e) => {
@@ -7,12 +14,8 @@ function DateSelector({ availableDatesArray, handleDateChange }) {
   };
   return (
     <div>
-      <select
-        value={availableDatesArray[availableDatesArray.length - 1]}
-        name=""
-        id=""
-        onChange={handleOnchange}
-      >
+      {/* <InputLabel htmlFor="select"></InputLabel> */}
+      <NativeSelect name="" id="" onChange={handleOnchange}>
         {availableDatesArray.map((date) => {
           return (
             <option key={availableDatesArray.indexOf(date)} value={date}>
@@ -20,7 +23,7 @@ function DateSelector({ availableDatesArray, handleDateChange }) {
             </option>
           );
         })}
-      </select>
+      </NativeSelect>
     </div>
   );
 }
